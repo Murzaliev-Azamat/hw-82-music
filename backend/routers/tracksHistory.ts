468fe87bac119c9a289e2483605d0b7ac85df7eb
Azamat Murzaliev <lirog.kg@gmail.com>
2023-02-17 02:38:35 +0600

    Немного почистил код

--- backend/routers/tracksHistory.ts
+++ backend/routers/tracksHistory.ts
@@ -1,53 +1,11 @@
 import express from 'express';
 import User from '../models/User';
-import mongoose, { Error } from 'mongoose';
-import {TrackHistoryMutation, TrackMutation} from "../types";
-import Track from "../models/Track";
+import mongoose from 'mongoose';
+import {TrackHistoryMutation} from "../types";
 import TrackHistory from "../models/TrackHistory";
-import trackHistory from "../models/TrackHistory";
 
 const tracksHistoryRouter = express.Router();
 
-// tracksHistoryRouter.post('/', async (req, res, next) => {
-//   try {
-//     const user = new User({
-//       username: req.body.username,
-//       password: req.body.password
-//     });
-//
-//     user.generateToken();
-//     await user.save();
-//     return res.send(user);
-//   } catch (error) {
-//     if (error instanceof Error.ValidationError) {
-//       return res.status(400).send(error);
-//     }
-//
-//     return next(error);
-//   }
-// });
-
-
-// usersRouter.post('/sessions', async (req, res) => {
-//   const user = await User.findOne({username: req.body.username});
-//
-//   if (!user) {
-//     return res.status(400).send({error: 'Username not found'});
-//   }
-//
-//   const isMatch = await user.checkPassword(req.body.password);
-//
-//   if (!isMatch) {
-//     return res.status(400).send({error: 'Password is wrong'});
-//   }
-//
-//   user.generateToken();
-//   await user.save();
-//
-//   return res.send({message: 'Username and password correct!', user});
-// });
-//
-//
 tracksHistoryRouter.post('/', async (req, res,next) => {
   const token = req.get('Authorization');
 
